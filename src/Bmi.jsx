765e62d397637d27367e58495bb8c70280d3cbe7
thamakorn dontreethai <thamakorn.do@ku.th>
2026-03-22 10:58:53 +0000

    refactor: remove App component and replace with Bmi component; add Bmi styles

---
+++ src/Bmi.jsx
@@ -0,0 +1,23 @@
+import './Bmi.css';
+import { useRef,useState } from "react";
+
+function BmiText({bmi}){
+    return 
+}
+export default function Bmi() {
+    const w_inputRef = useRef((null));
+    const h_inputRef = useRef((null));
+    const [bmi,setBmi]= useState(0);
+    function CalBmi(){
+        let w = parseFloat(w_inputRef.current.value);
+        let h = parseFloat(h_inputRef.current.value)/100.0;
+        setBmi(w/(h*h));
+    }
+    return(<><h1> Bmi calculator</h1>
+    weigth:<input ref={w_inputRef}/> kg.<br/>
+    height:<input ref={h_inputRef}/> cm.<br/>
+    <button onClick={()=>CalBmi()}>Calculate</button>
+    <p>bmi value: {bmi} </p>
+    <BmiText bmi ={bmi}/>
+    </>);
+}
